b6a840ee88857e0365a69f7b2fad7ca917e19afc
Patrik Dvoracek <patrik.dvoracek@PRGA-000009.local>
2019-11-23 18:27:43 +0100

    Basic structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Fake-a-Gram</title>
+        <meta content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        <div>
+            <div>
+                <img src="https://i.imgur.com/rSVeOIH.jpg" alt="memecko">
+            </div>
+        </div>
+    </body>
+</html>
