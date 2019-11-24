828d94229cd31de812fb2d3f7b38e3afd5fcf385
Patrik Dvoracek <patrik.dvoracek@PRGA-000009.local>
2019-11-24 11:26:50 +0100

    Moved light and dark theme into separate CSS files

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
     <head>
         <title>Fake-a-Gram</title>
         <meta content="text/html; charset=UTF-8">
+
+        <!-- Default CSS styling -->
         <link rel="stylesheet" href="/css/style.css">
+
+        <!-- Loads CSS styling based on system preference -->
+        <link rel="stylesheet" href="/css/darkstyle.css" media="(prefers-color-scheme: dark), (prefers-color-scheme: no-preference)">
+        <link rel="stylesheet" href="/css/lightstyle.css" media="(prefers-color-scheme: light)">
     </head>
     <body>
         <div>
