9c9a39e9e0d9783d6cff84c27697df7a6737460f
Patrik Dvoracek <patrik.dvoracek@PRGA-000009.rad.int.avast.com>
2019-12-03 18:18:26 +0100

    Finished DB communication

--- db_communication.php
+++ db_communication.php
@@ -18,15 +18,14 @@
 
     function add_user($conn, $username, $password, $passwordCheck, $email) {
         $sql = "SELECT username FROM users";
-        $result = $conn->query($sql); 
-        $usernames = $result->fetch_array();
+        $usernames = $conn -> query($sql) -> fetch_array();
 
         if($username > 31 || in_array($username, $usernames)) {
             return "Username is not valid";
         }
 
         $sql = "SELECT email FROM users";
-        $emails = $conn->query($sql)->fetch_array();
+        $emails = $conn -> query($sql) -> fetch_array();
 
         if($email > 255 || in_array($email, $emails)) {
             return "Email is not valid";
@@ -40,17 +39,17 @@
 
         $sql = "INSERT INTO users (username, pass, email) VALUES ('". $username ."', '". $pass ."', '". $email ."')";
 
-        $conn->query($sql);
+        $conn -> query($sql);
 
         return "Success";
     }
 
     function login($conn, $username, $password) {
         $sql = "SELECT pass FROM users WHERE username='". $username ."'";
-        $result = $conn->query($sql);
+        $result = $conn -> query($sql);
 
-        if($result->num_rows > 0) {
-            if(password_verify($password, $result->fetch_object()->pass)) {
+        if($result -> num_rows > 0) {
+            if(password_verify($password, $result -> fetch_object() -> pass)) {
                 return true;
             }
         }
@@ -61,26 +60,41 @@
     function add_post($conn, $title, $imgur_address, $username) {
         $user = get_user_id($conn, $username);
 
-        if($user >= 0) {
-            $sql = "INSERT INTO posts (imgur_address, title, user) VALUES ('". $imgur_address ."', '". $title ."', '". $user ."')";
-
-            $conn -> query($sql);
-
-            return "Success";
+        if($title === "") {
+            return "Invalid title";
         }
-        else {
+
+        if($imgur_address === "") {
+            return "Invalid url";
+        }
+
+        if($user < 1) {
             return "Invalid user";
         }
+
+        $sql = "INSERT INTO posts (imgur_address, title, user) VALUES ('". $imgur_address ."', '". $title ."', '". $user ."')";
+
+        $conn -> query($sql);
+
+        return "Success";
     }
 
     function get_user_id($conn, $username) {
         $sql = "SELECT id FROM users WHERE username='". $username ."'";
-        $result = $conn->query($sql);
+        $result = $conn->query($sql) -> fetch_object() -> id;
 
-        return $result->fetch_object()->id;
+        if($result > 0) {
+            return $result;
+        }
+
+        return -1;
     }   
 
     function get_posts($conn, $start, $count) {
+        if($start < 0 || $count < 1) {
+            return 0;
+        }
+
         $all_rows = $conn -> query("SELECT id FROM posts");
         $number = $count;
 
@@ -96,14 +110,15 @@
 
     function get_username($conn, $user_id) {
         $sql = "SELECT username FROM users WHERE id=". $user_id ." LIMIT 1";
-        $result = $conn -> query($sql);
-        return $result -> fetch_object() -> username;
+        return $conn -> query($sql) -> fetch_object() -> username;
     }
 
     function upvote($conn, $username, $post_id) {
-        $sql = 'INSERT INTO likes (post, user) VALUES ('. $post_id .', '. get_user_id($conn, $username) .')';
+        if($username !== "" && $post_id > 0) {
+            $sql = 'INSERT INTO likes (post, user) VALUES ('. $post_id .', '. get_user_id($conn, $username) .')';
 
-        $conn -> query($sql);
+            $conn -> query($sql);
+        }
     }
 
     function add_comment($conn, $post_id, $username, $comment) {
